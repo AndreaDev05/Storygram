2e046c49977d90731d4a80d995095636940c1c73
andree__ <depaandrea05@gmail.com>
2024-04-26 18:00:44 +0200

    aggiunta di un file per lo stoccaggio di tutte le credenziali necessarie

---
+++ credentials.py
@@ -0,0 +1,4 @@
+# File per il stoccaggio di tutti quei codici che si vogliono separare dal file principale
+
+#   chiave segreta usata da Flask per crittare i cookie da inviare al browser
+chiave_segreta = "63YUYAbo2xllzmOXQ36GwjTVmmS6URNt"
